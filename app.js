0e3438497f1f6bd728d992f67f397e2d39eeabf5
Albert Chang <a89529294@hotmail.com>
2024-05-29 13:00:49 +0800

    fix

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer(function(req,res){
-	res.write('On the way to being a fullstack engineer!');
-	res.end()
-})
+const server = http.createServer(function (req, res) {
+  if (req.url.startsWith("/api")) {
+    const responseObject = { message: "This is a JSON response" };
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      "access-control-allow-origin": "http://localhost:5173, http://acdev.lol",
+    });
+    res.end(JSON.stringify(responseObject));
+  } else {
+    res.write("On the way to being a fullstack engineer!");
+    res.end();
+  }
+});
 
 server.listen(3000);
 
-console.log('server started');
+console.log("server started");
