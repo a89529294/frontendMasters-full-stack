182d19185ac8d3277b5a3020bd30809f2720e64b
Albert Chang <a89529294@hotmail.com>
2024-05-30 17:35:05 +0800

    add sqlite

--- index-ws.js
+++ index-ws.js
@@ -11,23 +11,71 @@ server.listen(3000, function () {
   console.log("server started on port 3000");
 });
 
-/** web sockets */
+/** Begin websocket */
 const WebSocketServer = require("ws").Server;
-const wss = new WebSocketServer({ server });
 
-wss.on("connection", function (ws) {
-  const numClients = wss.clients.size;
+const wss = new WebSocketServer({ server: server });
 
-  console.log("clients connected: ", numClients);
-  wss.broadcast(`Current visitor count: ${numClients}`);
-
-  if (ws.readyState === ws.OPEN) ws.send("welcome to my server");
-  ws.on("close", function () {
-    wss.broadcast(`Current visitor count: ${numClients}`);
-    console.log("a client has disconnected");
+process.on("SIGINT", () => {
+  console.log("sigint");
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
   });
 });
 
-wss.broadcast = (msg) => {
-  wss.clients.forEach((ws) => ws.send(msg));
+wss.on("connection", function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log("Clients connected", numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to my server");
+  }
+
+  db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
+  ws.on("close", function close() {
+    wss.broadcast(`Current visitors: ${numClients}`);
+    console.log("A client has disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
 };
+
+/** end websockets */
+/** begin database */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+// no need to check if table exist because the db is in memory.
+db.serialize(() => {
+  db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  console.log("Shutting down db");
+
+  getCounts();
+  db.close();
+}
